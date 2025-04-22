ac5caed4b5dc6b38bb83ebc5a29e102c06c8751a
Ignacio <meloignacionicolas@gmail.com>
2025-04-22 11:23:51 -0300

    ADD: Estructura para backend

---
+++ backend/routes/alumnosRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const {
+	getAlumnos,
+	inscribirAlumno,
+} = require('../controllers/alumnosController');
+
+router.get('/', getAlumnos); // ACA NOSE SI VA '/' o '/alumnos' !!!!!!!!!!!!!
+router.post('/inscripcion', inscribirAlumno);
+
+module.exports = router;
