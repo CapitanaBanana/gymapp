621da85a4e4e61beee66daaeb7e1bae1e9aca9d6
Ignacio <meloignacionicolas@gmail.com>
2025-04-24 21:07:45 -0300

    ADD: listado deudores + logica back

--- backend/testdb.js
+++ backend/testdb.js
@@ -3,10 +3,10 @@ const pool = require('./db');
 const testDatabase = async () => {
 	try {
 		await pool.query(`
-        INSERT INTO cuotas (alumno_id, monto, adeuda, fecha_pago, tipo_cuota) VALUES
-    (1, 5000.00, FALSE, '2025-03-05', 'dos'),
-    (2, 7000.00, FALSE, '2025-04-10', 'tres'),
-    (3, 9000.00, TRUE, '2025-03-23', 'libre');
+        INSERT INTO cuotas (alumno_id, monto, fecha_pago, tipo_cuota) VALUES
+    (1, 5000.00, '2025-03-05', 'dos'),
+    (2, 7000.00, '2025-04-10', 'tres'),
+    (3, 9000.00, '2025-03-23', 'libre');
 		`);
 
 		console.log('✅ Base de datos reformada con exito.');
